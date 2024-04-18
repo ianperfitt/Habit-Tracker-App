6415c541c90f02cddb9eb9aca7ff57dd59bf2b9b
Ian Perfitt <iancp@protonnmail.com>
2024-04-17 19:17:46 -0500

    created Routes and updated app component to link to templates

--- egghead-angular/src/app/app.component.ts
+++ egghead-angular/src/app/app.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="container">
-    <h1>{{title}}</h1>
-    <app-habit-list></app-habit-list>
-  </div>
-  `,
+  templateUrl: 'app.component.html',
   styles: ['h1 { color: blue }']
 })
 export class AppComponent {
--- egghead-angular/src/app/app.module.ts
+++ egghead-angular/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'; 
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -6,18 +7,32 @@ import { AppComponent } from './app.component';
 import { HabitListComponent } from './habit-list/habit-list.component';
 import { HabitItemComponent } from './habit-item/habit-item.component';
 import { HabitFormComponent } from './habit-form/habit-form.component';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { AccountDetailComponent } from './account-detail/account-detail.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'account/:id', component: AccountDetailComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HabitListComponent,
     HabitItemComponent,
-    HabitFormComponent
+    HabitFormComponent,
+    HomeComponent,
+    AccountComponent,
+    AccountDetailComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
